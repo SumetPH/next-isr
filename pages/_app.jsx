ee0398d45dac78e80e4098747bd1aeb5aa9003a3
SumetPH <notsumet1@gmail.com>
2018-12-07 15:05:00 +0700

    Change title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,42 +1,42 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import App, { Container } from 'next/app'
-import withRedux from 'next-redux-wrapper'
-import { initStore, checkAdmin } from '../redux/store'
-import { ToastContainer } from 'react-toastify'
-import withNProgress from 'next-nprogress'
-import NProgressStyles from 'next-nprogress/styles'
+import React from "react";
+import { Provider } from "react-redux";
+import App, { Container } from "next/app";
+import withRedux from "next-redux-wrapper";
+import { initStore, checkAdmin } from "../redux/store";
+import { ToastContainer } from "react-toastify";
+// import withNProgress from 'next-nprogress'
+// import NProgressStyles from 'next-nprogress/styles'
 
 export default withRedux(initStore)(
-   class MyApp extends App {
-      static async getInitialProps({ Component, ctx }) {
-         return {
-            pageProps: Component.getInitialProps
-               ? await Component.getInitialProps(ctx)
-               : {}
-         }
-      }
+  class MyApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+      return {
+        pageProps: Component.getInitialProps
+          ? await Component.getInitialProps(ctx)
+          : {}
+      };
+    }
 
-      componentDidMount = () => {
-         if (localStorage.getItem('isAdmin')) {
-            this.props.store.dispatch(checkAdmin('login'))
-         }
+    componentDidMount = () => {
+      if (localStorage.getItem("isAdmin")) {
+        this.props.store.dispatch(checkAdmin("login"));
       }
+    };
 
-      render() {
-         const { Component, pageProps, store } = this.props
+    render() {
+      const { Component, pageProps, store } = this.props;
 
-         return (
-            <Container>
-               <Provider store={store}>
-                  <div>
-                     <NProgressStyles color="#fdcb6e" spinner={false} />
-                     <ToastContainer autoClose={2000} position="bottom-left" />
-                     <Component {...pageProps} />
-                  </div>
-               </Provider>
-            </Container>
-         )
-      }
-   }
-)
+      return (
+        <Container>
+          <Provider store={store}>
+            <div>
+              {/* <NProgressStyles color="#fdcb6e" spinner={false} /> */}
+              <ToastContainer autoClose={2000} position="bottom-left" />
+              <Component {...pageProps} />
+            </div>
+          </Provider>
+        </Container>
+      );
+    }
+  }
+);
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,38 +1,38 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-   static async getInitialProps(ctx) {
-      const initialProps = await Document.getInitialProps(ctx)
-      return { ...initialProps }
-   }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-   render() {
-      return (
-         <html>
-            <Head>
-               <meta charSet="utf-8" />
-               <meta
-                  name="viewport"
-                  content="width=device-width, initial-scale=1, shrink-to-fit=no"
-               />
-               <title>Mernstack ISR</title>
-               <link rel="shortcut icon" href="/static/icons/shortcut.png" />
-               <link rel="stylesheet" href="/static/css/style.css" />
-               <link rel="stylesheet" href="/static/css/bulma.css" />
-               <link rel="stylesheet" href="/static/css/animate.css" />
-               <link rel="stylesheet" href="/static/css/reactToastify.css" />
-               <link
-                  rel="stylesheet"
-                  href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
-                  integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
-                  crossOrigin="anonymous"
-               />
-            </Head>
-            <body>
-               <Main />
-               <NextScript />
-            </body>
-         </html>
-      )
-   }
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <title>NextJS ISR</title>
+          <link rel="shortcut icon" href="/static/icons/shortcut.png" />
+          <link rel="stylesheet" href="/static/css/style.css" />
+          <link rel="stylesheet" href="/static/css/bulma.css" />
+          <link rel="stylesheet" href="/static/css/animate.css" />
+          <link rel="stylesheet" href="/static/css/reactToastify.css" />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+            integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
 }
